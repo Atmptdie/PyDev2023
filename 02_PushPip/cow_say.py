bd486f717432ffc0decf9b95d7e7bc7e1fa483a6
Oleg <xor.oleg@yandex.ru>
2023-03-04 15:12:19 +0300

    Added some fixes
    -Message now isn't required and can be passed through the stdin
    -Fixed -n flag (default value is now similar to command line cowsay)

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 import cowsay
 import argparse
+import sys
 
 
 class CowsayArgumentParser(argparse.ArgumentParser):
@@ -19,7 +20,7 @@ parser = CowsayArgumentParser(prog="cowsay")
 parser.add_argument("-e", dest="eyes")
 parser.add_argument("-f", dest="cow")
 parser.add_argument("-l", dest="show_cow_list", action="store_true")
-parser.add_argument("-n", dest="wrap_text", action="store_true")
+parser.add_argument("-n", dest="wrap_text", action="store_false")
 parser.add_argument("-T", dest="tongue")
 parser.add_argument("-W", dest="width", type=int)
 parser.add_argument("-b", dest="preset", action="store_const", const="b")
@@ -30,9 +31,10 @@ parser.add_argument("-s", dest="preset", action="store_const", const="s")
 parser.add_argument("-t", dest="preset", action="store_const", const="t")
 parser.add_argument("-w", dest="preset", action="store_const", const="w")
 parser.add_argument("-y", dest="preset", action="store_const", const="y")
-parser.add_argument("message", nargs='+')
+parser.add_argument("message", nargs='*', default=sys.stdin)
 
 parsed_args = parser.parse_args().__dict__
+
 
 if parsed_args["show_cow_list"]:
     print(*cowsay.list_cows(), sep='\n')
