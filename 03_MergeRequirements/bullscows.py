8d29626a6fa97a155f81b9b8c21c9ec3b6d7e8ff
Oleg <xor.oleg@yandex.ru>
2023-03-09 20:36:39 +0300

    Added some fixes
    -Parsing command line arguments
    -Made complete gameplay loop with given parameters

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,4 +1,7 @@
 import random
+from urllib.request import urlopen
+import os
+import argparse
 
 
 def bullscows(guess: str, secret: str) -> "tuple[int, int]":
@@ -30,6 +33,7 @@ def bullscows(guess: str, secret: str) -> "tuple[int, int]":
 def ask(prompt: str, valid: "list[str]" = None) -> str:
     word = input(prompt)
     while not (valid is None or word in valid):
+        print(f"{word} is not a valid word! Try again\n")
         word = input(prompt)
 
     return word
@@ -42,13 +46,45 @@ def inform(format_string: str, bulls: int, cows: int) -> None:
 def gameplay(ask: callable, inform: callable, words: "list[str]") -> int:
     attempts = 0
     secret = random.choice(words)
-    word = ask("Введите слово: ", words)
+    word = ask("Enter word: ", words)
 
     while word != secret:
         bulls, cows = bullscows(word, secret)
-        inform("Быки: {}, Коровы: {}")
+        inform("Bulls: {}, Cows: {}", bulls, cows)
+        print("----------------------------\n")
 
-        word = ask("Введите слово: ", words)
+        word = ask("Enter word: ", words)
 
-    print(f"Да, это и правда '{secret}'. Количество попыток: {attempts}")
+    print(f"Congratulations! It was '{secret}'. Attempts: {attempts}")
 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="bullscows")
+
+    parser.add_argument("words_source")
+    parser.add_argument("length", type=int, default=5, nargs='?')
+
+    parsed_args = parser.parse_args()
+
+    words_source = parsed_args.words_source
+    length = parsed_args.length
+
+    is_local_file = os.path.isfile(words_source)
+    words = None
+    if is_local_file:
+        with open(words_source, "r", encoding='utf-8') as f:
+            words = f.readlines()
+    else:
+        print("Trying to open url...")
+        try:
+            with urlopen(words_source) as f:
+                words = f.read().decode().split("\n")
+                print("Successfully loaded words from url")
+        except ValueError:
+            raise ValueError("Source is neither a path nor URL. Try again!")
+
+    words = [word for word in words if len(word) == length]
+    if not words:
+        raise ValueError("There's no words with such length! Try again")
+    print(f"Starting game!")
+    gameplay(ask, inform, words)
